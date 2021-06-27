4421fcd80f42aa8a5fd2b692ebb1a106495814f3
Jacky Lao <laojackyy@icloud.com>
2021-06-27 15:35:30 +0800

    Add Landmark struct

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Jacky Lao on 6/27/21.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    private var coordinates: Coordinates
+
+    var image: Image {
+        Image(imageName)
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+}
